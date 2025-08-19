f01996ff96794a5317600e7e79fc5ba737dfa28c
ashutosh <ashutoshpro11@gmail.com>
2025-08-19 16:35:37 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -102,7 +102,7 @@ export default function Hero() {
         </p>
         <button
           className="mt-8 px-8 py-3 bg-cyan-500 rounded-full text-black font-semibold hover:bg-cyan-400 transition"
-          onClick={() => window.open("https://drive.google.com/your-resume-link", "_blank")}
+          onClick={() => window.open("https://drive.google.com/file/d/1-qAn_eirFYzVc2zg7cG6RTiZ3zlcWZji/view?usp=drive_link", "_blank")}
         >
           View Resume
         </button>
