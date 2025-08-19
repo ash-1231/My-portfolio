2db1c917214e12b50d66852801ec842a499441b8
ashutosh <ashutoshpro11@gmail.com>
2025-08-19 16:38:52 +0530

    Update Hero.jsx
    added hover effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -101,7 +101,7 @@ export default function Hero() {
           Building modern AI-powered web apps with passion and precision.
         </p>
         <button
-          className="mt-8 px-8 py-3 bg-cyan-500 rounded-full text-black font-semibold hover:bg-cyan-400 transition cursor-pointer"
+          className="mt-8 px-8 py-3 bg-cyan-500 rounded-full text-black font-semibold hover:bg-cyan-400 transition cursor:pointer"
           onClick={() => window.open("https://drive.google.com/file/d/1-qAn_eirFYzVc2zg7cG6RTiZ3zlcWZji/view?usp=sharing", "_blank")}
         >
           View Resume
